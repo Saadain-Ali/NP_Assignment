604174e39b69bda5393bbd0f4afb451e0f9253f8
saadain ali <ali.saadain@gmail.com>
2020-04-02 20:36:56 +0500

    NP Assignment First Commit

---
+++ NPClientServer_assignment2/NPClientServer_assignment2/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace NPClientServer_assignment2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.clientPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.grpRbtn = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(266, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Members";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FloralWhite;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(277, 221);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(71, 32);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Send";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(7, 212);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(266, 52);
+            this.textBox3.TabIndex = 14;
+            this.textBox3.Text = "Write a message";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(206, 19);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(174, 187);
+            this.textBox2.TabIndex = 13;
+            // 
+            // textBox1
+            // 
+            this.textBox1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.textBox1.Location = new System.Drawing.Point(7, 19);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(193, 187);
+            this.textBox1.TabIndex = 12;
+            // 
+            // clientPanel
+            // 
+            this.clientPanel.CausesValidation = false;
+            this.clientPanel.FlowDirection = System.Windows.Forms.FlowDirection.BottomUp;
+            this.clientPanel.Location = new System.Drawing.Point(386, 19);
+            this.clientPanel.Name = "clientPanel";
+            this.clientPanel.Size = new System.Drawing.Size(65, 187);
+            this.clientPanel.TabIndex = 18;
+            // 
+            // grpRbtn
+            // 
+            this.grpRbtn.AutoSize = true;
+            this.grpRbtn.Checked = true;
+            this.grpRbtn.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.grpRbtn.Location = new System.Drawing.Point(350, 212);
+            this.grpRbtn.Name = "grpRbtn";
+            this.grpRbtn.Size = new System.Drawing.Size(101, 17);
+            this.grpRbtn.TabIndex = 22;
+            this.grpRbtn.Text = "Group Message";
+            this.grpRbtn.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(455, 278);
+            this.Controls.Add(this.grpRbtn);
+            this.Controls.Add(this.clientPanel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.FlowLayoutPanel clientPanel;
+        private System.Windows.Forms.CheckBox grpRbtn;
+    }
+}
+
---
+++ NPClientServer_assignment2/NPClientServer_assignment2/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NPClientServer_assignment2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<TcpClient> clients = new List<TcpClient>(); //ToolBar store connected clients
+        List<CheckBox> boxes = new List<CheckBox>();    // TO store checkboxes for each clients
+        TcpListener server;
+        TcpClient activeClient;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            server = new TcpListener(IPAddress.Parse("192.168.0.86"), 8001); //My Servers IP
+            server.Start(10);
+            Thread t2 = new Thread(AcceptClient);
+            t2.Start();
+        }
+
+        //Accept clients
+
+        public void AcceptClient()
+        {
+            while (true)
+            {
+                TcpClient c = server.AcceptTcpClient();
+                clients.Add(c);
+                CheckBox b = checkBoxMaker(clients.Count.ToString());
+                this.BeginInvoke((Action)(() =>
+                {
+                    //perform on the UI thread
+                    this.clientPanel.Controls.Add(b);
+                }));
+                activeClient = c;
+                Thread t = new Thread(asd => ReadMessage(c));
+                t.Start();
+            }
+        }
+
+
+        //To Read Messages
+        public void ReadMessage(TcpClient client)
+        {
+            while (true)
+            {
+                TcpClient cur = clients.Single((x) => x == client);
+                NetworkStream stream = client.GetStream();
+                StreamReader sdr = new StreamReader(stream);
+                string msg = sdr.ReadLine();
+                textBox2.AppendText(Environment.NewLine);
+                textBox2.AppendText("Client" + (clients.IndexOf(cur)+1) + ": "  + msg);
+            }
+        }
+
+        ///send message
+        private void sendToAll(object sender, EventArgs e)
+        {
+            foreach (var item in clients)
+            {
+                textBox1.AppendText(Environment.NewLine);
+                textBox1.AppendText("Me: " + textBox3.Text);
+                NetworkStream stream = item.GetStream();
+                StreamWriter sdr = new StreamWriter(stream);
+                sdr.WriteLine(textBox3.Text);
+                sdr.Flush();
+            }
+        }
+
+
+        //Send Message to Selected Client
+        private void sendToActiveClient(object sender, EventArgs e)
+        {
+                textBox1.AppendText(Environment.NewLine);
+                textBox1.AppendText("Me: " + textBox3.Text);
+                NetworkStream stream = activeClient.GetStream();
+                StreamWriter sdr = new StreamWriter(stream);
+                sdr.WriteLine(textBox3.Text);
+                sdr.Flush();
+        }
+
+
+        
+
+        //Sends message to all client
+        public CheckBox checkBoxMaker(string name)
+        {
+            CheckBox chkBox = new CheckBox();
+            chkBox.AutoSize = true;
+            chkBox.Checked = true;
+            chkBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            //this.grpRbtn.Location = new System.Drawing.Point(279, 334);
+            chkBox.Name = "CBtn"+name;
+            chkBox.Size = new System.Drawing.Size(101, 17);
+            chkBox.TabIndex = 22;
+            chkBox.Text = "Client" + name;
+            chkBox.UseVisualStyleBackColor = true;
+            boxes.Add(chkBox);
+            return chkBox;
+        }
+
+        //A check to determine weither to send messages to a group of clients or a single client
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (clients.Count == 0)
+                MessageBox.Show("No client(s) selected", "Select CheckBoxes", MessageBoxButtons.OK);
+            else if (grpRbtn.Checked)
+                sendToAll(sender,e);
+            else
+            {
+                for (int i = 0; i < boxes.Count; i++)
+                {
+                    if (boxes[i].Checked)
+                    {
+                        activeClient = clients[i];
+                        sendToActiveClient(sender, e);
+                    }
+                }
+            }
+           
+            //if (grpRbtn.Checked)
+            //    sendToAll(sender, e);
+            //else
+            //    sendToActiveClient(sender,e);
+        }
+
+       
+    }
+}
